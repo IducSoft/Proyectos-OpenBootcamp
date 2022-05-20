534879781311c24dd1214a08f24474357bc1efce
isaac urdaneta <urdanetacuarteisaacdavid@gmail.com>
2022-05-20 05:57:44 -0400

    ootro ejercicio mas de vanilla js

---
+++ Ejercicio-tema-3-final.js
@@ -0,0 +1,2 @@
+let newList = ["Isaac Urdaneta", 26, true, new Date(29,09,1995), {autor: "charles Dickens", año: 1860, titulo: "grandes esperanzas", url: "https://es.wikipedia.org/wiki/Grandes_esperanzas"}];
+
