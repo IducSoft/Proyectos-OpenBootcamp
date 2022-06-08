82916734b8fd2d21619261a525c923e1d65acf47
isaac urdaneta <urdanetacuarteisaacdavid@gmail.com>
2022-06-07 20:18:48 -0400

    otro ejercicio de javascript

---
+++ ejercicio/Ejercicio-entregable-numeros.js
@@ -0,0 +1,6 @@
+let alturaCentimetros = 45,
+    alturaMetros = 34.65,
+    miPesoKiloGramos = 34.34,
+    alturaEnMetrosHaciaArriva = Math.round(82.6),
+    pesoRendondeadoHaciaAbajo = Math.floor(78.3),
+    maximoValor =`el máximo valor que se puede obtener en Javascript + 1 es: ${ Number.MAX_VALUE}`;
