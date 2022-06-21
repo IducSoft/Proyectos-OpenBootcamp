4443faf959996d4a7010876f869bd276509d80b1
isaac urdaneta <urdanetacuarteisaacdavid@gmail.com>
2022-06-21 10:17:57 -0400

    primer ejecricio completado de los sets

---
+++ conjunto.js
@@ -0,0 +1,9 @@
+// Creando i set
+
+const mySet1 = new Set();
+mySet1.add('maleisy cuarte');
+mySet1.add('isaric massiel');
+mySet1.add('victor colina');
+mySet1.add('isaac urdaneta yo');
+mySet1.add('isaac urdaneta yo');
+mySet1.add('javascript');
