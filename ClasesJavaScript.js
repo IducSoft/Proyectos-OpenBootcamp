748421b2be88fd37a0a3c38063b8e000bcf43c2d
isaac urdaneta <urdanetacuarteisaacdavid@gmail.com>
2022-07-15 21:43:36 -0400

    depuracion

--- ClasesJavaScript.js
+++ ClasesJavaScript.js
@@ -1,24 +1,14 @@
 class Estudiante {
-
     constructor(nombre){
-
         this.nombre = nombre;
         this.lista = ["HTML", "CSS", "JAVASCript"];
     }
-
-
     get obtenDatos(){
-
         return {
-
             nombre: this.nombre,
             asignaturas: this.lista
         }
     }
 }
-
-
 let student = new Estudiante("isaac");
-
-
-console.log(student.obtenDatos)
+console.log(student.obtenDatos);
---
+++ introDepuracion/index.js
@@ -0,0 +1,15 @@
+const persona = {
+
+    nombre: "isaac",
+    edad: 34
+}
+
+
+function multiplicarEdad(edad){
+
+    return 2 * edad;
+}
+
+let verEdad = multiplicarEdad(persona.edad);
+
+console.log(verEdad)
